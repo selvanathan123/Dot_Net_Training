5df83d9af642688fe9524acb56a19fd9a20e2bc0
“selva” <selvanathan@twilightsoftwares.com>
2023-11-15 19:26:24 +0530

    OOPS

---
+++ OOPS example/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using OOPS_example;
+using System;
+
+class OOPS
+{
+
+    // Main Method 
+    static public void Main(String[] args)
+    {
+
+        //Abstraction
+        Order Aorder = new Order();
+        Aorder.OrderNo = "1000";
+        Aorder.OrderCountry = "India";
+        Aorder.OrderType = "Fixed";
+
+        Aorder.CreateOrder();
+        Aorder.UpdateOrder();
+        Aorder.DeleteOrder();
+
+        Console.WriteLine();
+
+        //Encapsulation
+        Order Eorder = new Order();
+
+        Eorder.CreateOrder1();
+
+        Console.WriteLine();
+
+        Student stud = new Student();
+        stud.Firstname = "selva";
+        stud.Lastname = "nathan";
+
+        Details studd = new Details();
+        studd.Address = "Main Road";
+        studd.State = "Puducherry";
+        studd.Country = "India";
+        studd.StudentDetails();
+
+    }
+}
---
+++ OOPS example/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_example
+{
+    public class Student
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+
+    }
+
+    //Inheritance
+    public class Details: Student
+    {
+        public string? Address { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+
+        public void StudentDetails() 
+        {
+            Console.WriteLine("StudentDetails:" + Address);
+        
+        }
+
+    }
+
+
+}
